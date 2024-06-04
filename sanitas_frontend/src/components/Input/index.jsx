0de6e31da49414166b3103e32ca1374633bd16df
lemoonchild <ncastrom21@gmail.com>
2024-06-04 12:18:34 -0600

    fix: fixed types in input component

--- sanitas_frontend/src/components/Input/index.jsx
+++ sanitas_frontend/src/components/Input/index.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function Input({ type, value, onChange, placeholder }) {
-  return <input type={type} value={value} onChange={onChange} placeholder={placeholder} />;
+export default function Input({ type, value, onChange, name, checked, placeholder, label }) {
+  switch (type) {
+    case "radio":
+      return (
+        <label>
+          <input type={type} name={name} checked={checked} onChange={onChange} />
+          {label}
+        </label>
+      );
+
+    case "date":
+      return <input type={type} value={value} onChange={onChange} placeholder={placeholder} />;
+
+    default:
+      return <input type={type} value={value} onChange={onChange} placeholder={placeholder} />;
+  }
 }
